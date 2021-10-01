ffab8fd23195154927cc8b60081ba3d626bb3564
Angelo Polatto <polatto.xcode.job@gmail.com>
2021-09-30 21:28:15 -0300

    open door finished

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const { default: axios } = require('axios')
 const http = require('http')
 
 const host = "192.168.1.50"
-
 const user = "admin"
 const pass = "admin"
-const URL = "http://192.168.1.50"
 
+async function OpenDor (session){
+    await axios.post('http://' + host + '/execute_actions.fcgi?session=' + session, {
+        actions: [ { action: "sec_box", parameters: "id=65793, reason=3" } ]
+    })
+}
 
 async function requestSession(){
-    const data =  await axios.post('http://192.168.1.50/login.fcgi',{
+    const data =  await axios.post('http://' + host + '/login.fcgi',{
         login: user,
         password: pass
     })
         .then((response) => {
-            console.log(JSON.stringify(response.data))    
+            console.log(response.data.session)  
+            OpenDor(response.data.session)
+
         })
         .catch(erro => console.log(erro))
 }
